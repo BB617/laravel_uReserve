6d3dde63b2d35e40dcf1069ab40522e552a95e14
Kohei Takahashi <libimff0617@gmail.com>
2022-05-16 07:52:21 +0900

    event show

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Carbon\Carbon;
+use Laravel\Fortify\Actions\AttemptToAuthenticate;
+use League\CommonMark\Extension\Attributes\Parser\AttributesBlockContinueParser;
 
 class Event extends Model
 {
@@ -17,4 +21,25 @@ class Event extends Model
         'end_date',
         'is_visible',
     ];
+
+    public function eventDate(): Attribute
+    {
+        return new Attribute(
+            get: fn () => Carbon::parse($this->start_date)->format("Y年m月d日"),
+        );
+    }
+
+    public function startTime(): Attribute
+    {
+        return new Attribute(
+            get: fn () => Carbon::parse($this->start_date)->format("H時i分"),
+        );
+    }
+
+    public function endTime(): Attribute
+    {
+        return new Attribute(
+            get: fn () => Carbon::parse($this->end_date)->format("H時i分"),
+        );
+    }
 }
